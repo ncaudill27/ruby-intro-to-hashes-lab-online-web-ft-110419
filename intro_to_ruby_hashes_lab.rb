2dd9e18a058019f2b1c94ba20707b4f379ed3359
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-09 18:38:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,13 +8,19 @@ end
 
 def monopoly
 	monopoly = {
-	  railroads: {}
+	  railroads: {
+	    
+	  }
 	}
 
 end
 
 def monopoly_with_second_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+	monopoly = {
+	  railroads: {
+	    pieces: 4
+	  }
+	}
 
 end
 
