97a3d0eb1f57f022c731f44f0e00116927e1fe4a
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-09 18:46:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,8 +32,8 @@ def monopoly_with_third_tier
 	    pieces: 4,
 	    names: {},
 	    rent_in_dollars: {
-	      one_piece: 25,
-	      two_pieces: 50
+	      one_piece_owned: 25,
+	      two_pieces_owned: 50
 	    }
 	  }
 	}
